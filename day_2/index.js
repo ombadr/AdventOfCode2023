7234cffa7837c6874db8b15d3e96b763de63f5e2
Omar Badr <omarbadr938@gmail.com>
2023-12-05 21:07:34 +0100

    start day 3

--- day_2/index.js
+++ day_2/index.js
@@ -24,4 +24,33 @@ function partOne(file) {
     }, 0)
 }
 
-console.log(partOne('./input.txt'))
+// console.log(partOne('./input.txt'))
+
+
+function partTwo(file) {
+    const lines = fs.readFileSync(file, 'utf-8').trim().split('\n')
+    // Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+
+    return lines.map((line) => {
+
+        const maxCount = {
+            red: 0,
+            green: 0,
+            blue: 0
+        }
+        line.split(': ')[1].split('; ').forEach((set) => {
+            const pulls = set.split(', ')
+            return pulls.forEach((pull) => {
+                const [count, color] = pull.split(' ')
+                if (maxCount[color] < Number(count)) {
+                    maxCount[color] = Number(count)
+                }
+            })
+        })
+        return maxCount.red * maxCount.green * maxCount.blue
+    }).reduce((s, v) => {
+        return s + v
+    }, 0)
+}
+
+console.log(partTwo('./input.txt'))
---
+++ day_3/index.js
@@ -0,0 +1,10 @@
+import fs from 'fs'
+
+
+function partOne(file) {
+
+}
+
+function partTwo(file) {
+
+}
